a2b88b7026c2f07a13e8a09fc2ac94bf91d4b9c0
Shreyas <skraman1999@gmail.com>
2014-03-09 22:00:05 -0700

    Added play screen.

--- src/game/screens/PlayScreen.java
+++ src/game/screens/PlayScreen.java
@@ -3,16 +3,13 @@ package game.screens;
 import asciiPanel.AsciiPanel;
 import java.awt.event.KeyEvent;
 
-public class StartScreen implements Screen {
+public class PlayScreen implements Screen {
     
     public void displayOutput(AsciiPanel terminal) {
-        terminal.writeCenter("Dungeon Dive",2);
+        terminal.writeCenter("Play Screen",2);
     }
     
     public Screen respondToUserInput(KeyEvent key) {
-        char k = key.getKeyChar();
-        if (k == KeyEvent.VK_ENTER)
-            return new PlayScreen();
         return this;
     }
 }
