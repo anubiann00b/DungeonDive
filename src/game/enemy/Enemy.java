10ae78500a5adf728c95069281c1c65997c122d3
Shreyas <skraman1999@gmail.com>
2014-03-10 21:52:50 -0700

    Enemy is beautiful.

--- src/game/enemy/Enemy.java
+++ src/game/enemy/Enemy.java
@@ -13,5 +13,23 @@ public class Enemy {
     private String name;
     private Color color;
     private World world;
-    private int visionRadius;
+    
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getMaxHp() { return maxHp; }
+    public int getCurrentHp() { return currentHp; }
+    public char getGlyph() { return glyph; }
+    public String getName() { return name; }
+    public Color getColor() { return color; }
+    
+    public Enemy(int x, int y, int hp, char glyph, String name, Color color, World world, int visionRadius) {
+        this.x = x;
+        this.y = y;
+        this.maxHp = hp;
+        this.currentHp = hp;
+        this.glyph = glyph;
+        this.name = name;
+        this.color = color;
+        this.world = world;
+    }
 }
